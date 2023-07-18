6ea9a190db0310186cd68879232da5b00bac090e
Sunit Corneleous <sunit.corneleous25@gmail.com>
2023-07-18 11:42:45 +0600

    spinner in button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import Loader from "./../assets/spinner.svg";
 
-const Button = ({ title, customClass, actionHandler, buttonType }) => {
+const Button = ({ title, customClass, actionHandler, buttonType, loading }) => {
+  let content = title;
+
+  if (loading) {
+    content = <img src={Loader} className="w-full h-6" />;
+  } else {
+    content = title;
+  }
+
   return (
     <button
       type={buttonType}
       onClick={actionHandler}
-      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${customClass}`}
+      className={`${
+        loading ? "bg-gray-400" : "bg-blue-500 hover:bg-blue-700"
+      } text-white font-bold py-2 px-4 rounded ${customClass}`}
     >
-      {title}
+      {content}
     </button>
   );
 };
